1ef0957dc1cfbe5de1c81eacc0ff58777eb27b2f
Takeshi KOYAMA <mayatake29@gmail.com>
2017-10-02 22:42:35 +0900

    first commit

---
+++ Assets/script/WebcamCapture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamCapture : MonoBehaviour {
+
+	private WebCamTexture webcamtex;
+
+	// Use this for initialization
+	void Start () {
+		webcamtex = new WebCamTexture ();
+
+		Renderer _renderer = GetComponent<Renderer> ();
+		_renderer.material.mainTexture = webcamtex;
+		webcamtex.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
